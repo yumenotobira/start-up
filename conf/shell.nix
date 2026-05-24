10acac8e3a24ff57de509472c40938f7cb943436
y <grnggm@gmail.com>
2026-05-24 18:01:10 +0900

    Add zsh setting

--- conf/shell.nix
+++ conf/shell.nix
@@ -7,5 +7,15 @@
     enableCompletion = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
+
+    initContent = ''
+      HISTSIZE=10000
+      SAVEHIST=10000
+      HISTFILE="$HOME/.zsh_history"
+
+      setopt AUTO_CD
+      setopt HIST_IGNORE_DUPS
+      setopt SHARE_HISTORY
+    '';
   };
 }
